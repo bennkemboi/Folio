7107d1623d406c8ab872d2cb9db7602d4c583744
bennkemboi <benkemboi23@gmail.com>
2023-08-14 14:28:19 +0300

    first commit

---
+++ assets/data.js
@@ -0,0 +1,18 @@
+const servicesData = [
+  {
+    title: "Frontend Development",
+    category: "frontend",
+    description: ["Lorem30 "],
+  },
+  {
+    title: "Backend Development",
+    category: "backend",
+    description: ["Lorem30 ewwff "],
+  },
+  {
+    title: "Fullstack Development",
+    category: "fullstack",
+    description: ["Lorem30 "],
+  },
+];
+
